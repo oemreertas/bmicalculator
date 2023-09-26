d88493f6c9303e6814cc1d9f5945d38b53044ec2
Özgür Emre Ertaş <oemreertas@gmail.com>
2023-09-26 20:23:28 +0300

    indeks_show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 
+
+
 '-------------------window-----------'
 
 window = Tk()
@@ -17,24 +19,34 @@ y = (screen_height - window.winfo_reqheight()) / 2
 window.geometry("+%d+%d" % (x, y))
 '-------------------------------------'
 '---------------------------BMI FUNCTION-------------------'
+
+
 def BMIFunction(*args):
     try:
         value1 = float(entry1.get())
-        value2 = float(entry2.get())/100
-        sonuc = value1/(value2*value2)
+        value2 = float(entry2.get()) / 100
+        sonuc = value1 / (value2 * value2)
+        print(sonuc)
 
         if sonuc < 18.5:
             label4.config(text=f"Underweight :(")
+            label5.config(text=f"{sonuc}")
         elif 18.5 < sonuc < 24.9:
             label4.config(text=f"Normal weight :)")
+            label5.config(text=f"{sonuc}")
         elif 25 < sonuc < 29.9:
             label4.config(text=f"Overweight :/")
+            label5.config(text=f"{sonuc}")
         elif 30 < sonuc < 34.9:
             label4.config(text=f"Obesity!")
+            label5.config(text=f"{sonuc}")
         elif sonuc > 35:
             label4.config(text=f"Extrame Obesity!!!")
+            label5.config(text=f"{sonuc}")
     except ValueError:
-            messagebox.showerror("Error", "An invalid number entry was completed.")
+        messagebox.showerror("Error", "An invalid number entry was completed.")
+
+
 '----------------------------------------------------------'
 
 '----------------------LABELS----------------------'
@@ -51,8 +63,12 @@ label3.place(x=150, y=225, anchor="center")
 label4 = Label(window)
 label4.config(background="#1c1c1c", font=new_font, fg="White")
 label4.place(x=150, y=255, anchor="center")
+label5 = Label(window)
+label5.config(background="#1c1c1c", font=new_font, fg="White")
+label5.place(x=150, y=200, anchor="center")
 '---------------------------------------------------'
 '-------------------Entry---------------------------'
+
 
 def entryClick(event):
     if entry1.get() == "For exp:75":
@@ -61,6 +77,7 @@ def entryClick(event):
                       fg="White",
                       font=("Arial", 10, "bold"))
         print(entry1.get())
+
 
 def entryClick2(event):
     if entry2.get() == "For exp:170":
@@ -97,7 +114,6 @@ entry2.config(background="#1c1c1c",
               insertbackground="White",
               insertborderwidth=5)
 
-
 '---------------------------------------------------'
 '---------------------BUTTON------------------------'
 button1 = Button(window, text="Calculator", command=BMIFunction)
@@ -111,3 +127,4 @@ button1.config(borderwidth=2,
 button1.place(x=150, y=170, anchor="center")
 button1.bind()
 window.mainloop()
+
